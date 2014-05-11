68e970ec9f2e1a6c77e6f943a9f67599b6b085c0
Stephen Reid <postscript07@gmail.com>
2014-05-11 21:50:45 +0100

    .

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,44 @@
+$(function() {
+
+  $('textarea.wysiwyg').each(function() {
+    var textarea = this;
+    var summernote = $('<div class="summernote"></div>');
+    $(summernote).insertAfter(this);
+    $(summernote).summernote({height: 500});
+    $(summernote).code($(textarea).val());
+    $('.note-fontname, .note-help', summernote.next()).hide();
+    $('button[data-event=codeview]', summernote.next()).click();
+    $(textarea).hide();
+    $(textarea.form).submit(function() {
+      $(textarea).val($(summernote).code());
+    });
+  });
+
+  $('input[type=text].slug').each(function() {
+    var slug = $(this);
+    var start_length = slug.val().length;
+    var pos = $.inArray(this, $('input', this.form)) - 1;
+    var title = $($('input', this.form).get(pos));
+    slug.focus(function() {
+      slug.data('focus', true);
+    });
+    title.keyup(function() {
+      if (start_length == 0 && slug.data('focus') != true)
+        slug.val(title.val().toLowerCase().replace(/ /g, '-').replace(/[^a-z0-9\-]/g, ''));
+    });
+  });
+
+  $(document).on('click', 'a[data-confirm]', function(e) {
+    var message = $(this).data('confirm');
+    if (!confirm(message)) {
+      e.preventDefault();
+      e.stopped = true;
+    }
+  });
+
+  $(document).on('click', 'a.popup', function(e) {
+    window.open(this.href, null, 'width=600,height=300,left=150,top=150').focus();
+    return false;
+  });
+
+});
--- app/assets/javascripts/jquery.confirm.js
+++
@@ -1,6 +0,0 @@
-$(function() { 
-  $("a[data-confirm]").on('click', function(e) {
-    var message = $(this).data('confirm');
-    if (!confirm(message)) { e.preventDefault(); e.stopped = true; }
-  });
-});
