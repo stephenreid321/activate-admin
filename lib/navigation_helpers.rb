bcd68538819f6b60959d8fa41e72860eb68693b2
Stephen Reid <postscript07@gmail.com>
2014-05-11 14:15:16 +0100

    .

--- lib/navigation_helpers.rb
+++ lib/navigation_helpers.rb
@@ -5,13 +5,20 @@ module Activate
       s = ''
       s << %Q{<ul class="#{css_class}">}      
       items.each { |name, path|
-        s << '<li'
-        s << %Q{ class="active" } if request.path == path
-        s << '>'
-        s << %Q{<a href="#{path}">#{name}</a>}
-        s << '</li>'
+        if path.is_a? Array
+          s << %Q{<li class="dropdown">}
+          s << %Q{<a data-toggle="dropdown" class="dropdown-toggle" href="javascript:;">#{name} <b class="caret"></b></a>}
+          s << ul_nav('dropdown-menu', path)
+          s << %Q{</li>}
+        else
+          s << %Q{<li}
+          s << %Q{ class="active" } if request.path == path
+          s << %Q{>}
+          s << %Q{<a href="#{path}">#{name}</a>}
+          s << %Q{</li>}
+        end
       }
-      s << '</ul>'
+      s << %Q{</ul>}
     end
     
   end
