3d29e294106de01861e49e9a3a9ae9f8c4832028
Stephen Reid <postscript07@gmail.com>
2015-12-03 16:22:00 +0000

    .

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -12,17 +12,17 @@ ActivateAdmin::App.helpers do
     params[:model].constantize
   end  
     
-  def admin_fields(model, recursive: false)
+  def admin_fields(model)
     admin_fields = model.admin_fields
     admin_fields[:created_at] = {:type => :datetime, :edit => false} if persisted_field?(model, :created_at)
     admin_fields[:updated_at] = {:type => :datetime, :edit => false} if persisted_field?(model, :updated_at)
-    admin_fields = Hash[admin_fields.map { |fieldname, options|
-        options = {:type => options} if options.is_a?(Symbol)
+    admin_fields = Hash[admin_fields.map { |fieldname, options|        
+        options = {:type => options} if options.is_a?(Symbol)        
         options[:index] = true if !options.keys.include?(:index) and index_types.include?(options[:type])
         options[:edit] = true if !options.keys.include?(:edit)
         options[:disabled] = true if fieldname == :id    
-        options[:class_name] = assoc(model, fieldname, relationship: case options[:type]; when :lookup; :belongs_to; when :collection; :has_many; end).class_name if [:lookup, :collection].include?(options[:type])
-        [fieldname, options[:type] == :collection && recursive ? admin_fields(options[:class_name].constantize, recursive: true) : options]
+        options[:class_name] = assoc(model, fieldname, relationship: case options[:type]; when :lookup; :belongs_to; when :collection; :has_many; end).class_name if [:lookup, :collection].include?(options[:type])        
+        [fieldname, options]
       }]
     admin_fields[admin_fields.first.first][:lookup] = true if !admin_fields.find { |fieldname, options| options[:lookup] }
     admin_fields
