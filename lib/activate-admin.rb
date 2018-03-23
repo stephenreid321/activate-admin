b43e6caaf7a9ffb80bdbe2a7a51c9d7fafbb421f
Stephen Reid <postscript07@gmail.com>
2018-03-23 18:53:58 +0000

    .

--- lib/activate-admin.rb
+++ lib/activate-admin.rb
@@ -9,27 +9,3 @@ module ActivateAdmin
   extend Padrino::Module
   gem! "activate-admin"
 end
-
-if defined? Delayed::Job
-  module Delayed
-    module Backend
-      module Mongoid
-        class Job
-          def self.admin_fields
-            {
-              :priority => :number,
-              :attempts => :number,
-              :handler => :text_area,            
-              :run_at => :datetime,
-              :locked_at => :datetime,
-              :locked_by => :text,
-              :failed_at => :datetime,
-              :last_error => :text_area,
-              :queue => :text
-            }
-          end
-        end
-      end
-    end
-  end
-end
